381260ff3cb44556023ba01df69712cd054fc900
PAULO RENATO ALVES FIRMINO <43581747+praf62@users.noreply.github.com>
2022-08-15 12:55:48 -0300

    VS Code configuration for R and Python

--- P/script01_estatisticaDescritiva.py
+++ P/script01_estatisticaDescritiva.py
@@ -1,5 +1,11 @@
-print("ol[a], mundo")
-#import pandas as pd
-#import numpy as np
+#print("ol[a], mundo")
+#in python terminal (Windows):
+#pip install pandas
+#in python terminal (macOS/Linux):
+#pip3 install pandas
+from base64 import encode
+import pandas as pd
+# import numpy as np
 #read file
-#PBD01 = pd.read_csv read.csv(file = "D:/OneDrive - Universidade Federal do Cariri - UFCA/Drive/UFCA/Ensino/CRAN R_aulas/RClasses/data/BD01.csv", dec = ".", encoding="UTF-8", sep=";", quote="")#Praf's computer
+PBD01 = pd.read_csv (filepath_or_buffer= "D:/OneDrive - Universidade Federal do Cariri - UFCA/Drive/UFCA/Ensino/CRAN R_aulas/RClasses/data/BD01.csv", decimal= ".", encoding="UTF-8", delimiter=";")#Praf's computer
+PBD01.head()
---
+++ R/RConfigInVSCode.r
@@ -0,0 +1,9 @@
+install.packages("languageserver")
+#library(languageserver) # nolint
+#?languageserver
+
+#in python terminal (Windows):
+#pip install -U radian
+#in python terminal (macOS/Linux):
+#pip3 install -U radian
+#To update, run: python.exe -m pip install --upgrade pip
