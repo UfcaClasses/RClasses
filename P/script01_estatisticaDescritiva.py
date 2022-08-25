92cea385288a525258e7244afc7bdd23fb68a1f1
PAULO RENATO ALVES FIRMINO <43581747+praf62@users.noreply.github.com>
2022-08-25 15:35:53 -0300

    titanic case

--- P/script01_estatisticaDescritiva.py
+++ P/script01_estatisticaDescritiva.py
@@ -5,12 +5,13 @@
 #! pip3 install pandas#??
 #from base64 import encode
 import pandas as pd
-from IPython.display import display
+#from IPython.display import display
 # import numpy as np
 #read file
-PBD01 = pd.read_csv (filepath_or_buffer= "D:/OneDrive - Universidade Federal do Cariri - UFCA/Drive/UFCA/Ensino/CRAN R_aulas/RClasses/data/BD01.csv", decimal= ".", encoding="UTF-8", delimiter=";")#Praf's computer
+#PBD01 = pd.read_csv (filepath_or_buffer= "D:/OneDrive - Universidade Federal do Cariri - UFCA/Drive/UFCA/Ensino/CRAN R_aulas/RClasses/data/BD01.csv", decimal= ".", encoding="UTF-8", delimiter=";")#Praf's computer
+PBD01 = pd.read_csv (filepath_or_buffer= "./data/BD01.csv", decimal= ".", encoding="UTF-8", delimiter=";")#Praf's computer or VS Code
 PBD01.head()
-display(PBD01)
+#display(PBD01)
 PBD01.describe()
 #pip install jinja2
 #import jinja2
